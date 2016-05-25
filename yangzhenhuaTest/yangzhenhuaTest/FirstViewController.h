d3a6460cc6a9bd434bad3fc6a89cd1ee299b4a4f
260869781 <260869781@qq.com>
2016-05-25 15:57:50 +0800

    创建了FirstViewController

---
+++ yangzhenhuaTest/yangzhenhuaTest/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  yangzhenhuaTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 yangzhenhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
--- yangzhenhuaTest/yangzhenhuaTest/ViewController.m
+++ yangzhenhuaTest/yangzhenhuaTest/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int a = 1;
+    int sum = a++;
+    NSLog(@"%d",sum);
 }
 
 - (void)didReceiveMemoryWarning {
